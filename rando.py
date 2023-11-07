3d841808d37ba361b4075fd526358fa75539f2c9
hackerbotsupreme <ALOKE.GEEK@gmail.com>
2023-11-07 10:59:56 +0530

    first commit

---
+++ rando.py
@@ -0,0 +1,9 @@
+# making a calculator
+import math
+a=int(input('enter the first number :'))
+b=int(input("enter the second number :"))
+x=input("enter the operator:")
+if x=="+":
+    print("the result of the operation is ",a+b)
+elif x=="-":
+    print("the result of the operation is",a-b)
